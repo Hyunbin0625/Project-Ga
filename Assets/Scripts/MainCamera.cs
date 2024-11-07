b283a20ce29749faa82ee4a6e076b0b3ed92bbc3
KJH\rlawo <rlawogjs147258@gmail.com>
2024-11-07 16:36:41 +0900

    camera player좌표 따라 이동 및 회전 script 추가

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Transform target;    // Player 정보
+    public float offsetZ;       // Player와 카메라 Z 거리
+    public float smoothSpeed = 0.125f; // 카메라의 딜레이 속도
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+
+        // 카메라 회전 = player 회전
+        transform.rotation = target.rotation;
+
+    }
+}
